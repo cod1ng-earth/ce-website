a2c2b54144bf1151e3b57dc10409bedbf77f1051
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-09-14 23:53:05 +0200

    status overlay

--- src/components/MeetupPreview.js
+++ src/components/MeetupPreview.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
-import { Box, Button, Grid, Heading, Image, Paragraph, Text } from "grommet"
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  Image,
+  Paragraph,
+  Stack,
+  Text,
+} from "grommet"
 import { Map } from "grommet-icons"
 import styled from "styled-components"
 
@@ -25,35 +34,53 @@ export default ({ group, meetup }) => {
     >
       <HoverBox
         background="dark-1"
-        margin={{ vertical: `small` }}
-        pad="small"
+        margin={{ vertical: `medium` }}
         round="xxsmall"
         full={true}
         flex={true}
         alignSelf="center"
       >
-        <Grid columns={[`auto`, `1/4`]}>
-          <Box>
-            <Text>
-              {date.toFormat(`ccc`)},{` `}
-              {date.toLocaleString(DateTime.DATETIME_MED)}
-            </Text>
-            <Heading level={4} margin={{ bottom: `xsmall` }}>
-              {meetup.name}
-            </Heading>
+        <Stack anchor="top-right">
+          <Grid
+            columns={[`auto`, `1/4`]}
+            rows={[`auto`, `auto`]}
+            areas={[
+              { name: `title`, start: [0, 0], end: [1, 1] },
+              { name: `img`, start: [1, 0], end: [1, 0] },
+              { name: `desc`, start: [0, 1], end: [1, 1] },
+            ]}
+          >
+            <Box gridArea="title" pad="small">
+              <Text>
+                {date.toFormat(`ccc`)},{` `}
+                {date.toLocaleString(DateTime.DATETIME_MED)}
+              </Text>
+              <Heading full level={4} margin={{ bottom: `xsmall` }}>
+                {meetup.name}
+              </Heading>
 
-            <Text>
-              <Map size="medium" /> {meetup.venue && meetup.venue.name}
-            </Text>
-          </Box>
-          <Box height="xsmall">
-            <Image fit="cover" src={group.key_photo.photo_link} />
-          </Box>
-        </Grid>
+              <Text>
+                <Map size="medium" /> {meetup.venue && meetup.venue.name}
+              </Text>
+            </Box>
+            <Box gridArea="img" height="170px" pad="small">
+              <Image fit="cover" src={group.key_photo.photo_link} />
+            </Box>
 
-        <Paragraph size="small" fill>
-          {excerpt(meetup.description, { pruneLength: 280 })}
-        </Paragraph>
+            <Box gridArea="desc" size="small" pad="small">
+              <Paragraph fill>
+                {excerpt(meetup.description, { pruneLength: 500 })}
+              </Paragraph>
+            </Box>
+          </Grid>
+          <Box
+            background={meetup.status === `upcoming` ? `brand` : `dark-3`}
+            pad={{ vertical: `xxsmall`, horizontal: `medium` }}
+            style={{ marginTop: `-1.5rem` }}
+          >
+            <Text weight="bold">{meetup.status}</Text>
+          </Box>
+        </Stack>
       </HoverBox>
     </Link>
   )
