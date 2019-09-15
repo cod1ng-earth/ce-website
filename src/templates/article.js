9adf2d4193b267d19cb88d80882a6fb7bc385420
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-09-15 23:04:24 +0200

    lint problems

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,17 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Box, Grid, Heading, Image, Paragraph, Text } from "grommet"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-import ResponsiveGrid from "../components/ResponsiveGrid"
-import styled from "styled-components"
-import { theme } from "../components/theme"
-import { DateTime } from "luxon"
-
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from "react-responsive-carousel"
 
 export default ({ data }) => (
   <Layout>
