135366cd6ff4b2d49b6d7fef62358cbb7e0b7df7
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-09-29 20:02:56 +0200

    contact form -> netlify

--- src/components/footer/ContactForm.js
+++ src/components/footer/ContactForm.js
@@ -1,26 +1,49 @@
 import React, { useState } from "react"
+import styled from "styled-components"
 
-import { Button, Form, FormField, TextArea } from "grommet"
+import { Button, Form, FormField, TextArea, Paragraph } from "grommet"
+
+const HiddenField = styled.input`
+  display: "none";
+  height: 0;
+`
 
 export default () => {
   const [submitted, setSubmitted] = useState(false)
-  const [message, setMessage] = React.useState(``)
+  const [message, setMessage] = useState(``)
 
-  const formSubmitted = evt => {
-    setSubmitted(true)
-  }
-
-  return (
-    <Form>
+  return submitted ? (
+    <Paragraph> Thank you</Paragraph>
+  ) : (
+    <Form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      netlify-honeypot="important-note-field"
+    >
       <FormField name="email" type="email" placeholder="Your@emailaddre.ss" />
+      <HiddenField>
+        <label>
+          Another field for you to fill: <input name="important-note-field" />
+        </label>
+      </HiddenField>
+
       <TextArea
+        name="message"
         focusIndicator={true}
         resize="vertical"
         placeholder="your message"
         value={message}
         onChange={e => setMessage(e.target.value)}
       />
-      {!submitted && <Button type="submit" primary label="Submit" />}
+      <Button
+        type="submit"
+        color="brand"
+        fill
+        primary
+        label="Submit"
+        margin={{ top: `medium` }}
+      />
     </Form>
   )
 }
