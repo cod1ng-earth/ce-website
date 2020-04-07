d0024ede1f7f22422b03905c06b0aa6e1b408f27
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-04-07 21:47:24 +0200

    adds Auth0 auth shows login button on page header

--- src/components/header/AppHeader.js
+++ src/components/header/AppHeader.js
@@ -1,8 +1,10 @@
 import { Link } from 'gatsby'
-import { Anchor, Box, Image, ResponsiveContext } from 'grommet'
+import { Anchor, Avatar, Box, Button, Image } from 'grommet'
+import { Github } from 'grommet-icons'
 import React from 'react'
 import styled from 'styled-components'
 import logo from '../../images/coding_earth_x_120.png'
+import { useAuth0 } from '../auth/react-auth0-spa'
 import { theme } from '../theme'
 
 const StyledAnchor = styled(Anchor)`
@@ -14,24 +16,57 @@ const StyledAnchor = styled(Anchor)`
   }
 `
 
-export default () => (
-  <ResponsiveContext.Consumer>
-    {size => (
-      <Box
-        tag="header"
-        direction="row"
-        background="very-dark"
-        align="center"
-        justify="center"
-        margin={{ vertical: 'medium' }}
-        pad={{ horizontal: 'medium' }}
-        style={{ position: 'relative' }}
-        height="xsmall"
-      >
+export default () => {
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+
+    user,
+  } = useAuth0()
+
+  return (
+    <Box
+      tag="header"
+      direction="row"
+      background="very-dark"
+      align="center"
+      justify="between"
+      margin={{ vertical: 'medium' }}
+      pad={{ horizontal: 'medium' }}
+      style={{ position: 'relative' }}
+      height="xsmall"
+    >
+      <Box>
         <Link to="/">
           <Image src={logo} alt="coding earth logo" />
         </Link>
       </Box>
-    )}
-  </ResponsiveContext.Consumer>
-)
+      <Box>
+        {!isAuthenticated && (
+          <Button
+            color="black"
+            primary
+            icon={<Github />}
+            label="Log in"
+            onClick={() => loginWithRedirect({})}
+          />
+        )}
+
+        {isAuthenticated && (
+          <Button
+            color="dark-2"
+            primary
+            icon={
+              user && (
+                <Avatar size="small" background="black" src={user.picture} />
+              )
+            }
+            label="Log out"
+            onClick={() => logout({ returnTo: 'https://coding.earth' })}
+          />
+        )}
+      </Box>
+    </Box>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import PropTypes from 'prop-types'
-
+import { graphql, navigate, useStaticQuery } from 'gatsby'
 import { Box, Grommet } from 'grommet'
-import { theme } from './theme'
-
-import AppHeader from './header/AppHeader'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Auth0Provider } from './auth/react-auth0-spa'
 import AppFooter from './footer/AppFooter'
 import SubFooter from './footer/SubFooter'
+import AppHeader from './header/AppHeader'
+import { theme } from './theme'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -20,23 +19,34 @@ const Layout = ({ children }) => {
     }
   `)
 
-  const [showSidebar, setShowSidebar] = useState(false)
+  //const [showSidebar, setShowSidebar] = useState(false)
+
+  const onRedirectCallback = appState => {
+    navigate(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    )
+  }
+
   return (
-    <Grommet theme={theme} themeMode="dark">
-      <Box background="black">
-        <AppHeader
-          appName="coding earth"
-          onToggleSidebar={() => {
-            setShowSidebar(!showSidebar)
-          }}
-        />
-        <Box full align="center">
-          {children}
+    <Auth0Provider
+      domain={process.env.GATSBY_AUTH0_DOMAIN}
+      client_id={process.env.GATSBY_AUTH0_CLIENT_ID}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <Grommet theme={theme} themeMode="dark">
+        <Box background="black">
+          <AppHeader appName="coding earth" />
+          <Box full align="center">
+            {children}
+          </Box>
+          <AppFooter />
+          <SubFooter />
         </Box>
-        <AppFooter />
-        <SubFooter />
-      </Box>
-    </Grommet>
+      </Grommet>
+    </Auth0Provider>
   )
 }
 
