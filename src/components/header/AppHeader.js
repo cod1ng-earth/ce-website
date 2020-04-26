a2e7d37d0a434b982f37029749d976c02df8ec77
Lars Eichler <lars.eichler@turbinekreuzberg.com>
2020-04-26 19:32:47 +0200

    Hero Styling

--- src/components/header/AppHeader.js
+++ src/components/header/AppHeader.js
@@ -1,19 +1,29 @@
 import { Link } from 'gatsby'
-import { Anchor, Avatar, Box, Button, Image } from 'grommet'
+import { Anchor, Avatar, Box, Button, Image, Nav } from 'grommet'
 import { Github } from 'grommet-icons'
 import React from 'react'
 import styled from 'styled-components'
-import logo from '../../images/coding_earth_x_120.png'
+import logo from '../../images/icons/ce-logo.svg'
 import { useAuth0 } from '../auth/react-auth0-spa'
 import { theme } from '../theme'
 
-const StyledAnchor = styled(Anchor)`
-  font-family: 'OCR-A';
-  color: ${theme.global.colors.brand};
+const LinkStyles = `
+  color: ${theme.global.colors.white};
   text-decoration: none;
+  font-weight: 600;
+  font-size: 14px;
+  
   :hover {
     color: ${theme.global.colors.turqoise};
+    text-decoration: none;
   }
+`
+
+const StyledLink = styled(Link)`
+  ${LinkStyles}
+`
+const StyledAnchor = styled(Anchor)`
+  ${LinkStyles}
 `
 
 export default () => {
@@ -23,20 +33,33 @@ export default () => {
     <Box
       tag="header"
       direction="row"
-      background="very-dark"
+      background="grey-800"
       align="center"
       justify="between"
-      margin={{ vertical: 'medium' }}
+      margin={{ top: '20px' }}
       pad={{ horizontal: 'medium' }}
-      style={{ position: 'relative' }}
+      style={{ position: 'relative', height: '80px' }}
       height="xsmall"
     >
       <Box>
         <Link to="/">
-          <Image src={logo} alt="coding earth logo" />
+          <Image
+            src={logo}
+            alt="coding earth logo"
+            style={{
+              width: '87px',
+              height: '100px',
+              display: 'block',
+            }}
+          />
         </Link>
       </Box>
-      <Box>
+      <Box direction="row" align="center">
+        <Nav direction="row" margin={{ right: 'medium' }}>
+          <StyledLink to="/">Upcoming Events</StyledLink>
+          <StyledLink to="/sofar">Previous Events</StyledLink>
+          <StyledAnchor>Our Rules</StyledAnchor>
+        </Nav>
         {!isAuthenticated && (
           <Button
             color="black"
