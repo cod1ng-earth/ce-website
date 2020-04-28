5fbe8dc6a8dd9d4ce5b3820fb61fa525530e66e1
patricia.ribeiro <patricia.ribeiro@turbinekreuzberg.com>
2020-04-28 15:57:09 +0100

    Fix issues with header styles - remove use of styled components

--- src/components/header/AppHeader.js
+++ src/components/header/AppHeader.js
@@ -6,6 +6,7 @@ import styled from 'styled-components'
 import logo from '../../images/ce-logo.svg'
 import { useAuth0 } from '../auth/react-auth0-spa'
 import { theme } from '../theme'
+import { HeaderWrapper } from './HeaderWrapper'
 
 const LinkStyles = `
   color: ${theme.global.colors.white};
@@ -26,30 +27,21 @@ const StyledAnchor = styled(Anchor)`
   ${LinkStyles}
 `
 
-const StyledWrapper = styled(Box)`
-  background: ${props =>
-    props.inHero === true ? 'transparent' : theme.global.colors['grey-800']};
-  justify-content: ${props =>
-    props.inHero === true ? 'flex-end' : 'space-between'};
-  position: ${props => (props.inHero === true ? 'absolute' : 'relative')};
-  height: 80px;
-`
-
 export default ({ currentPath }) => {
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0()
+  const isInHero = currentPath === '/'
+
+  const headerProps = isInHero
+    ? {
+        background: 'transparent',
+        justify: 'end',
+        style: { position: 'absolute', height: '80px' },
+      }
+    : {}
 
   return (
-    <StyledWrapper
-      tag="header"
-      direction="row"
-      align="center"
-      margin={{ top: '20px' }}
-      pad={{ horizontal: 'medium' }}
-      height="xsmall"
-      fill="horizontal"
-      inHero={currentPath === '/'}
-    >
-      {currentPath !== '/' && (
+    <HeaderWrapper {...headerProps}>
+      {!isInHero && (
         <Box>
           <Link to="/">
             <Image
@@ -94,6 +86,6 @@ export default ({ currentPath }) => {
           />
         )}
       </Box>
-    </StyledWrapper>
+    </HeaderWrapper>
   )
 }
