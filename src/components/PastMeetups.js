9ed9544b98e35f036b1bb4c747decb726f291922
patricia.ribeiro <patricia.ribeiro@turbinekreuzberg.com>
2020-04-30 19:42:53 +0100

    Style past meetups section

--- src/components/PastMeetups.js
+++ src/components/PastMeetups.js
@@ -1,15 +1,20 @@
-import Image from 'gatsby-image'
-import { Box, Heading, Paragraph, Text, Button } from 'grommet'
-import { Rewind } from 'grommet-icons'
-import React from 'react'
-import { theme } from './theme'
-import { FullWidth } from './TwoCols'
-import { graphql, useStaticQuery, Link, navigate } from 'gatsby'
+import React, { useContext } from 'react'
+import { Box, Heading, Text, Image, ResponsiveContext } from 'grommet'
+import { Calendar } from 'grommet-icons'
+import { graphql, useStaticQuery, navigate } from 'gatsby'
+import styled from 'styled-components'
 import Time from './Time'
 
-const colors = theme.global.colors
+const StyledH3 = styled(Heading)`
+  font-size: 20px;
+  line-height: 30px;
+  margin-block-start: 0;
+  margin-block-end: 0;
+`
 
 export default function() {
+  const breakpoint = useContext(ResponsiveContext)
+
   const data = useStaticQuery(graphql`
     query {
       graphcms {
@@ -42,34 +47,44 @@ export default function() {
   `)
 
   const meetups = data.graphcms.meetups
+
   return (
-    <Box pad={{ vertical: 'medium' }} direction="row-responsive" wrap={true}>
+    <Box
+      direction="row-responsive"
+      wrap={true}
+      margin={{ horizontal: '-10px', top: 'medium', bottom: 'large' }}
+      width={{ max: 'none' }}
+    >
       {meetups.map(meetup => (
         <Box
-          pad="small"
-          basis="1/2"
+          basis={`${breakpoint === 'medium' ? '1/2' : '1/3'}`}
           key={meetup.id}
           align="center"
-          responsive={true}
+          pad="10px"
           onClick={() => navigate(`meetup/${meetup.id}`)}
         >
           <Box
-            pad="small"
-            fill
-            height={{ min: 'medium' }}
-            background={{ image: `url(${meetup.highlightImage.url})` }}
-            align="start"
-            justify="end"
+            height="200px"
+            round={{ size: '4px', corner: 'top' }}
+            overflow="hidden"
           >
-            <Box
-              background={{ color: 'dark-1', opacity: true }}
-              fill="horizontal"
-              pad="small"
-            >
-              <Heading level={3} color="brand" margin="none">
-                {meetup.name}
-              </Heading>
-              <Text color="white" size="medium">
+            <Image fit="cover" fill src={meetup.highlightImage.url} />
+          </Box>
+          <Box
+            background={{ color: 'grey-700', opacity: true }}
+            fill="horizontal"
+            pad={{ vertical: 'small', horizontal: 'medium' }}
+            justify="between"
+            gap="small"
+            height="115px"
+            round={{ size: '4px', corner: 'bottom' }}
+          >
+            <StyledH3 level={3} color="white" margin="none">
+              {meetup.name}
+            </StyledH3>
+            <Box direction="row" align="center">
+              <Calendar color="orange-400" size="20px" />
+              <Text color="grey-100" size="xsmall" margin={{ left: 'small' }}>
                 {Time({ timeString: meetup.time })}
               </Text>
             </Box>
